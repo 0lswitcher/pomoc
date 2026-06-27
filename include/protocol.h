f78c3a8e17dc2b60e5108f6f1d25cf4b83685d88
cobra-r9 <cobra.rev.9@gmail.com>
2026-06-27 10:49:16 +0530

    added protocol.c and protocol.h

---
+++ include/protocol.h
@@ -0,0 +1,39 @@
+// file : protocol.h 
+
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+// socket path
+extern const char *SOCKET_PATH;
+
+// commands to start pause and end the pomodoro. 
+extern const char *CMD_START;
+extern const char *CMD_PAUSE;
+extern const char *CMD_END;
+
+// client commands to get various status of the pomod daemon. 
+extern const char *CMD_STATUS;
+extern const char *CMD_STATUS_ACT;
+extern const char *CMD_STATUS_TIME;
+extern const char *CMD_STATUS_HR;
+extern const char *CMD_STATUS_MIN;
+extern const char *CMD_STATUS_SEC;
+
+// client commands to set the time for focus and break, override the configfile. 
+extern const char *CMD_SET_FOCUS;
+extern const char *CMD_SET_BREAK;
+
+// client commands to increment the time for focus or break.
+extern const char *CMD_INCR;
+
+// the response strings for OK and ERR. 
+extern const char *RES_OK;
+extern const char *RES_ERR;
+
+// standard buffer size. 
+extern const int BUF_SIZE;
+
+#endif
+
+// tip : alternatively, we can also use #pragma once. (modern. )
+
---
+++ src/protocol.c
@@ -0,0 +1,37 @@
+// name : protocol.c 
+// date : 2026-06-27 
+// std  : C 23 standard 
+
+#include "protocol.h"
+
+// socket path
+const char *SOCKET_PATH = "/tmp/pomo.socket";
+
+// commands to start pause and end the pomodoro. 
+const char *CMD_START = "START";
+const char *CMD_PAUSE = "PAUSE";
+const char *CMD_END = "END";
+
+// client commands to get various status of the pomod daemon. 
+const char *CMD_STATUS = "STATUS";
+const char *CMD_STATUS_ACT = "STATUS active";
+const char *CMD_STATUS_TIME = "STATUS time";
+const char *CMD_STATUS_HR = "STATUS hr";
+const char *CMD_STATUS_MIN = "STATUS min";
+const char *CMD_STATUS_SEC = "STATUS sec";
+
+// client commands to set the time for focus and break, override the configfile. 
+const char *CMD_SET_FOCUS = "SET focus";
+const char *CMD_SET_BREAK = "SET break";
+
+// client commands to increment the time for focus or break.
+const char *CMD_INCR = "INCR";
+
+// the response strings for OK and ERR. 
+const char *RES_OK = "OK";
+const char *RES_ERR = "ERR";
+
+// standard buffer size. 
+const int BUF_SIZE = 256;
+
+
